9f2cf846b7607e90b70a15975c37ffdded734748
DCI - Marc <marcel.saegebarth@dci-student.org>
2025-07-10 12:39:48 +0200

    Add application configuration

---
+++ src/main/java/org/dcistudent/sakilarest/SakilaRestApplication.java
@@ -0,0 +1,13 @@
+package org.dcistudent.sakilarest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SakilaRestApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SakilaRestApplication.class, args);
+  }
+
+}
