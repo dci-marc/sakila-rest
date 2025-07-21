903f665ffda375ced2cba3e57989d7bb3927b2e7
DCI - Marc <marcel.saegebarth@dci-student.org>
2025-07-21 14:30:51 +0200

    Implement `FilmManager` with pagination

---
+++ src/main/java/org/dcistudent/sakilarest/managers/FilmManager.java
@@ -0,0 +1,35 @@
+package org.dcistudent.sakilarest.managers;
+
+import org.dcistudent.sakilarest.entities.Film;
+import org.dcistudent.sakilarest.repositories.FilmRepository;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FilmManager {
+
+  @NotNull
+  private final FilmRepository filmRepository;
+
+  public FilmManager(@NotNull FilmRepository filmRepository) {
+    this.filmRepository = filmRepository;
+  }
+
+  public @NotNull Page<Film> findAll(@NotNull Integer limit, @NotNull Integer offset) {
+    Pageable pageable = PageRequest.of(offset, limit);
+    return this.filmRepository.findAll(pageable);
+  }
+
+  public @NotNull List<Film> findByTitle(@NotNull String title) {
+    return this.filmRepository
+        .findFilmsByTitle(title)
+        .orElseThrow(() ->
+          new IllegalArgumentException("No films found with title: " + title)
+        );
+  }
+}
