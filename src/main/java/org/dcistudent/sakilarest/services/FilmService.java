74dd2b2c11000d3d3b1045ac74f3adcaba6b04be
DCI - Marc <marcel.saegebarth@dci-student.org>
2025-07-21 14:30:25 +0200

    Implement `FilmService` for film list

---
+++ src/main/java/org/dcistudent/sakilarest/services/FilmService.java
@@ -0,0 +1,33 @@
+package org.dcistudent.sakilarest.services;
+
+import org.dcistudent.sakilarest.entities.Film;
+import org.dcistudent.sakilarest.managers.FilmManager;
+import org.dcistudent.sakilarest.models.responses.FilmResponse;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.stereotype.Service;
+
+import java.time.ZoneId;
+import java.util.List;
+
+@Service
+public class FilmService {
+
+  final @NotNull FilmManager filmManager;
+
+  public FilmService(@NotNull FilmManager filmManager) {
+    this.filmManager = filmManager;
+  }
+
+  public @NotNull List<FilmResponse> getFilms(@NotNull Integer limit, @NotNull Integer offset) {
+    return this.filmManager.findAll(limit, offset)
+        .map(film -> new FilmResponse(
+            film.getTitle(),
+            film.getDescription(),
+            film.getReleaseYear(),
+            film.getLength(),
+            film.getRating(),
+            film.getSpecialFeatures(),
+            film.getLastUpdate().atZone(ZoneId.systemDefault())
+        )).toList();
+  }
+}
