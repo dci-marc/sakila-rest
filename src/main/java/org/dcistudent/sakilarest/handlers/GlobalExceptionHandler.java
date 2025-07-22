4c2e2711fba99acf60b4c7629436eb2382d1e5a4
DCI - Marc <marcel.saegebarth@dci-student.org>
2025-07-22 15:35:10 +0200

    Fix response body for invalid arguments

--- src/main/java/org/dcistudent/sakilarest/handlers/GlobalExceptionHandler.java
+++ src/main/java/org/dcistudent/sakilarest/handlers/GlobalExceptionHandler.java
@@ -4,14 +4,15 @@ import org.dcistudent.sakilarest.factories.ResponseFactory;
 import org.dcistudent.sakilarest.loggers.SqlLogger;
 import org.dcistudent.sakilarest.models.Response;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.Arrays;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -41,17 +42,20 @@ public class GlobalExceptionHandler {
   }
 
   @ExceptionHandler(MethodArgumentNotValidException.class)
-  public @NotNull Response<String> handleValidation(@NotNull MethodArgumentNotValidException ex) {
+  public @NotNull Response<List<Map<String, String>>> handleValidation(MethodArgumentNotValidException ex) {
+    List<Map<String, String>> errors = ex.getBindingResult().getFieldErrors().stream()
+        .map(err -> {
+          Map<String, String> errorDetail = new HashMap<>();
+          errorDetail.put("field", err.getField());
+          errorDetail.put("message", Objects.requireNonNull(err.getDefaultMessage()));
+          return errorDetail;
+        })
+        .toList();
+
     return ResponseFactory.create(
         Response.Status.BAD_REQUEST.get(),
         "error:validation:fail",
-        ex.getBindingResult().getFieldErrors().stream()
-            .map(err -> Map.of(
-                "field", err.getField(),
-                "message", Objects.requireNonNull(err.getDefaultMessage())
-            ))
-            .toList()
-            .toString()
+        errors
     );
   }
 }
