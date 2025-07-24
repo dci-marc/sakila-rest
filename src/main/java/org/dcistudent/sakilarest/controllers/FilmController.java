e0707526345257f0f3adf001b1da80dd89e414c4
DCI - Marc <marcel.saegebarth@dci-student.org>
2025-07-24 15:42:23 +0200

    Implement route search for `FilmController`

--- src/main/java/org/dcistudent/sakilarest/controllers/FilmController.java
+++ src/main/java/org/dcistudent/sakilarest/controllers/FilmController.java
@@ -2,16 +2,12 @@ package org.dcistudent.sakilarest.controllers;
 
 import jakarta.validation.Valid;
 import org.dcistudent.sakilarest.factories.ResponseFactory;
-import org.dcistudent.sakilarest.models.Paging;
 import org.dcistudent.sakilarest.models.Response;
 import org.dcistudent.sakilarest.models.requests.FilmRequest;
 import org.dcistudent.sakilarest.models.responses.FilmResponse;
 import org.dcistudent.sakilarest.services.FilmService;
 import org.jetbrains.annotations.NotNull;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,35 +21,35 @@ public class FilmController {
     this.filmService = filmService;
   }
 
-  @PostMapping("")
-  public @NotNull Response<List<FilmResponse>> getFilms(@NotNull @RequestBody @Valid Paging paging) {
+  @GetMapping("/{id}")
+  public @NotNull Response<Object> getFilm(@NotNull @PathVariable Integer id) {
     try {
       return ResponseFactory.create(
           Response.Status.OK.get(),
           "film:fetch:success",
-          this.filmService.getFilms(paging.getLimit(), paging.getOffset())
+          this.filmService.getFilm(id.longValue())
       );
     } catch (IllegalArgumentException e) {
       return ResponseFactory.create(
           Response.Status.BAD_REQUEST.get(),
           "film:fetch:not.found",
-          List.of()
+          new Object()
       );
     }
   }
 
-  @PostMapping("/by-title")
-  public @NotNull Response<List<FilmResponse>> getFilms(@NotNull @RequestBody @Valid FilmRequest request) {
+  @GetMapping
+  public @NotNull Response<List<FilmResponse>> getFilms(@NotNull @ModelAttribute @Valid FilmRequest request) {
     try {
       return ResponseFactory.create(
           Response.Status.OK.get(),
-          "film:fetch:by.title:success",
-          this.filmService.getFilmsByTitle(request.getTitle())
+          "films:fetch:success",
+          this.filmService.routeSearch(request)
       );
     } catch (IllegalArgumentException e) {
       return ResponseFactory.create(
           Response.Status.BAD_REQUEST.get(),
-          "film:fetch:by.title:not.found",
+          "films:fetch:not.found",
           List.of()
       );
     }
