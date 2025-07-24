aa317a4d26dc9c5286c2adc428e11e5c3d6c6f99
DCI - Marc <marcel.saegebarth@dci-student.org>
2025-07-24 15:41:24 +0200

    Implement `FilmRepository`

--- src/main/java/org/dcistudent/sakilarest/repositories/FilmRepository.java
+++ src/main/java/org/dcistudent/sakilarest/repositories/FilmRepository.java
@@ -2,6 +2,8 @@ package org.dcistudent.sakilarest.repositories;
 
 import org.dcistudent.sakilarest.entities.Film;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,5 +13,14 @@ import java.util.Optional;
 @Repository
 public interface FilmRepository extends PagingAndSortingRepository<Film, Long> {
 
-  @NotNull Optional<List<Film>> findByTitleContaining(@NotNull String pattern);
+  @NotNull Optional<Film> findById(@NotNull Long id);
+  @NotNull Optional<List<Film>> findByTitleContainingIgnoreCase(
+      @NotNull String pattern,
+      @NotNull Pageable pageable
+  );
+  @Query("SELECT f FROM Film f WHERE LOWER(CAST(f.description AS string)) LIKE LOWER(CONCAT('%', :pattern, '%'))")
+  @NotNull Optional<List<Film>> findByDescription(
+      @NotNull String pattern,
+      @NotNull Pageable pageable
+  );
 }
