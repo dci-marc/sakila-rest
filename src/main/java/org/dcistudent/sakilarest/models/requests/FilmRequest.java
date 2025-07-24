a8686eeeac96d2640ea61316dcde7d9f9696f54c
DCI - Marc <marcel.saegebarth@dci-student.org>
2025-07-24 15:40:59 +0200

    Implement `FilmRequest`

--- src/main/java/org/dcistudent/sakilarest/models/requests/FilmRequest.java
+++ src/main/java/org/dcistudent/sakilarest/models/requests/FilmRequest.java
@@ -2,23 +2,19 @@ package org.dcistudent.sakilarest.models.requests;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
 import org.jetbrains.annotations.NotNull;
 
-public class FilmRequest {
+@Getter
+@Setter
+public class FilmRequest extends LimitOffsetRequest {
 
   @NotBlank
   @Min(value = 3, message = "Title must be at least 3 characters long.")
-  private String title;
+  private @NotNull String title = "";
 
-  public FilmRequest(@NotNull String title) {
-    this.title = title;
-  }
-
-  public String getTitle() {
-    return title;
-  }
-
-  public void setTitle(String title) {
-    this.title = title;
-  }
+  @NotBlank
+  @Min(value = 3, message = "Description must be at least 3 characters long.")
+  private @NotNull String description = "";
 }
