16d6661546b03b7062edb17fa2623a8567d7bcf4
DCI - Marc <marcel.saegebarth@dci-student.org>
2025-07-25 12:09:38 +0200

    Implement release year for rquest

--- src/main/java/org/dcistudent/sakilarest/models/requests/FilmRequest.java
+++ src/main/java/org/dcistudent/sakilarest/models/requests/FilmRequest.java
@@ -2,6 +2,7 @@ package org.dcistudent.sakilarest.models.requests;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import org.jetbrains.annotations.NotNull;
@@ -17,4 +18,9 @@ public class FilmRequest extends LimitOffsetRequest {
   @NotBlank
   @Min(value = 3, message = "Description must be at least 3 characters long.")
   private @NotNull String description = "";
+
+  @NotNull
+  @Size(min = 4, max = 4, message = "Release year must be a 4-digit number.")
+  @Min(value = 1850, message = "Release year must be after 1850.")
+  private Integer releaseYear = 0;
 }
