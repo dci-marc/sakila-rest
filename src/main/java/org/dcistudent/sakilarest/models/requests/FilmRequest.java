20997410d7aced09db0337e05a8f4bb2254d049d
DCI - Marc <marcel.saegebarth@dci-student.org>
2025-07-28 14:09:44 +0200

    Remove Bean validation

--- src/main/java/org/dcistudent/sakilarest/models/requests/FilmRequest.java
+++ src/main/java/org/dcistudent/sakilarest/models/requests/FilmRequest.java
@@ -1,8 +1,5 @@
 package org.dcistudent.sakilarest.models.requests;
 
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import org.jetbrains.annotations.NotNull;
@@ -11,16 +8,7 @@ import org.jetbrains.annotations.NotNull;
 @Setter
 public class FilmRequest extends LimitOffsetRequest {
 
-  @NotBlank
-  @Min(value = 3, message = "Title must be at least 3 characters long.")
   private @NotNull String title = "";
-
-  @NotBlank
-  @Min(value = 3, message = "Description must be at least 3 characters long.")
   private @NotNull String description = "";
-
-  @NotNull
-  @Size(min = 4, max = 4, message = "Release year must be a 4-digit number.")
-  @Min(value = 1850, message = "Release year must be after 1850.")
-  private Integer releaseYear = 0;
+  private @NotNull Integer releaseYear = 0;
 }
