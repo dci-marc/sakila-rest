4367cc6f59f95183f30ec8e62a1292299dea0c98
DCI - Marc <marcel.saegebarth@dci-student.org>
2025-07-30 12:31:16 +0200

    Remove unused repository `CustomerList...`

--- src/main/java/org/dcistudent/sakilarest/repositories/CustomerListRepository.java
+++
@@ -1,7 +0,0 @@
-package org.dcistudent.sakilarest.repositories;
-
-import org.dcistudent.sakilarest.entities.CustomerList;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerListRepository extends JpaRepository<CustomerList, Long> {
-}
