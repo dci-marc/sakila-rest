02cf90eaf569f284d525988857ad74471e471037
DCI - Marc <marcel.saegebarth@dci-student.org>
2025-07-30 16:30:50 +0200

    Implement native query for customers

--- src/main/java/org/dcistudent/sakilarest/repositories/CustomerRepository.java
+++ src/main/java/org/dcistudent/sakilarest/repositories/CustomerRepository.java
@@ -1,7 +1,14 @@
 package org.dcistudent.sakilarest.repositories;
 
 import org.dcistudent.sakilarest.entities.Customer;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.NativeQuery;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+  @NativeQuery(value = "SELECT * FROM customer WHERE address_id >= 100")
+  @NotNull Page<Customer> findCustomersAddressAbove100(@NotNull Pageable pageable);
 }
