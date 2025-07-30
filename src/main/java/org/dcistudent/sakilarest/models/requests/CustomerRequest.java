53d605c867e9a0b7dd6aca2759eac44d363b8c0e
DCI - Marc <marcel.saegebarth@dci-student.org>
2025-07-30 16:31:09 +0200

    Implement `CustomerRequest` model

---
+++ src/main/java/org/dcistudent/sakilarest/models/requests/CustomerRequest.java
@@ -0,0 +1,14 @@
+package org.dcistudent.sakilarest.models.requests;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.jetbrains.annotations.NotNull;
+
+@Getter
+@Setter
+public class CustomerRequest extends LimitOffsetRequest {
+
+  private @NotNull String title = "";
+  private @NotNull String description = "";
+  private @NotNull Integer releaseYear = 0;
+}
