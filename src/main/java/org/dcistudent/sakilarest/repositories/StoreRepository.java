23c1fa76c7a987e60d18a5a15de53045ce5a794e
DCI - Marc <marcel.saegebarth@dci-student.org>
2025-07-31 14:23:19 +0200

    Implement `findAll` & use entity graph annotation

--- src/main/java/org/dcistudent/sakilarest/repositories/StoreRepository.java
+++ src/main/java/org/dcistudent/sakilarest/repositories/StoreRepository.java
@@ -2,11 +2,17 @@ package org.dcistudent.sakilarest.repositories;
 
 import org.dcistudent.sakilarest.entities.Store;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface StoreRepository extends JpaRepository<Store, Long> {
 
+  @NotNull Page<Store> findAll(@NotNull Pageable pageable);
+
+  @EntityGraph(value = "Store.eager", type = EntityGraph.EntityGraphType.LOAD)
   @NotNull Optional<Store> findById(@NotNull Long id);
 }
