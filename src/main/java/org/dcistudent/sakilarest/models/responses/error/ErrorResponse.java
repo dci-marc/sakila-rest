c16a7d82efea66ccec7a2281c24354f09006783f
DCI - Marc <marcel.saegebarth@dci-student.org>
2025-07-31 13:12:35 +0200

    Make `ErrorResponse` a record

--- src/main/java/org/dcistudent/sakilarest/models/responses/error/ErrorResponse.java
+++ src/main/java/org/dcistudent/sakilarest/models/responses/error/ErrorResponse.java
@@ -1,15 +1,7 @@
 package org.dcistudent.sakilarest.models.responses.error;
 
-import lombok.Getter;
 import org.dcistudent.sakilarest.models.responses.ResponsePayload;
 import org.jetbrains.annotations.NotNull;
 
-@Getter
-public final class ErrorResponse implements ResponsePayload {
-
-  private final @NotNull String message;
-
-  public ErrorResponse(@NotNull String message) {
-    this.message = message;
-  }
+public record ErrorResponse(@NotNull String message) implements ResponsePayload {
 }
