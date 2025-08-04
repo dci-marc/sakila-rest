4b425e930175d99cdb3be3138a92ec193dd35e3f
DCI - Marc <marcel.saegebarth@dci-student.org>
2025-08-04 13:16:36 +0200

    Implement base REST exceptions

---
+++ src/main/java/org/dcistudent/sakilarest/exceptions/BadRequestException.java
@@ -0,0 +1,12 @@
+package org.dcistudent.sakilarest.exceptions;
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class BadRequestException extends RuntimeException {
+  public BadRequestException(@NotNull String message) {
+    super(message);
+  }
+}
