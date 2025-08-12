b72218a51d35468c0e1cfdc5f228dd5895c2a9f0
DCI - Marc <marcel.saegebarth@dci-student.org>
2025-08-12 15:43:31 +0200

    Cache repositories

--- src/main/java/org/dcistudent/sakilarest/repositories/CustomerRepository.java
+++ src/main/java/org/dcistudent/sakilarest/repositories/CustomerRepository.java
@@ -2,13 +2,17 @@ package org.dcistudent.sakilarest.repositories;
 
 import org.dcistudent.sakilarest.entities.Customer;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.NativeQuery;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
+@Cacheable("customers")
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
   @NativeQuery(value = "SELECT * FROM customer WHERE address_id >= 100")
--- src/main/java/org/dcistudent/sakilarest/repositories/StoreRepository.java
+++ src/main/java/org/dcistudent/sakilarest/repositories/StoreRepository.java
@@ -2,13 +2,17 @@ package org.dcistudent.sakilarest.repositories;
 
 import org.dcistudent.sakilarest.entities.Store;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
+@Cacheable("stores")
 public interface StoreRepository extends JpaRepository<Store, Long> {
 
   @NotNull Page<Store> findAll(@NotNull Pageable pageable);
