485498aee39df27ac2fe96938be36cb43f0587f3
DCI - Marc <marcel.saegebarth@dci-student.org>
2025-08-14 15:18:25 +0200

    Remove database as prefix

--- src/main/resources/db/migration/mariadb/V1__init.sql
+++ src/main/resources/db/migration/mariadb/V1__init.sql
@@ -414,10 +414,10 @@ a.first_name,
 a.last_name,
 GROUP_CONCAT(DISTINCT CONCAT(c.name, ': ',
 		(SELECT GROUP_CONCAT(f.title ORDER BY f.title SEPARATOR ', ')
-                    FROM sakila.film f
-                    INNER JOIN sakila.film_category fc
+                    FROM film f
+                    INNER JOIN film_category fc
                       ON f.film_id = fc.film_id
-                    INNER JOIN sakila.film_actor fa
+                    INNER JOIN film_actor fa
                       ON f.film_id = fa.film_id
                     WHERE fc.category_id = c.category_id
                     AND fa.actor_id = a.actor_id
@@ -425,12 +425,12 @@ GROUP_CONCAT(DISTINCT CONCAT(c.name, ': ',
              )
              ORDER BY c.name SEPARATOR '; ')
 AS film_info
-FROM sakila.actor a
-LEFT JOIN sakila.film_actor fa
+FROM actor a
+LEFT JOIN film_actor fa
   ON a.actor_id = fa.actor_id
-LEFT JOIN sakila.film_category fc
+LEFT JOIN film_category fc
   ON fa.film_id = fc.film_id
-LEFT JOIN sakila.category c
+LEFT JOIN category c
   ON fc.category_id = c.category_id
 GROUP BY a.actor_id, a.first_name, a.last_name;
 
