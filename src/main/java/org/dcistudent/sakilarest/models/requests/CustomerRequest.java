6c1fe011fa9c7fc767e3236474d14549d62ec334
DCI - Marc <marcel.saegebarth@dci-student.org>
2025-08-15 17:56:35 +0200

    Delombok request models

--- src/main/java/org/dcistudent/sakilarest/models/requests/CustomerRequest.java
+++ src/main/java/org/dcistudent/sakilarest/models/requests/CustomerRequest.java
@@ -1,14 +1,34 @@
 package org.dcistudent.sakilarest.models.requests;
 
-import lombok.Getter;
-import lombok.Setter;
 import org.jetbrains.annotations.NotNull;
 
-@Getter
-@Setter
 public class CustomerRequest extends LimitOffsetRequest {
 
   private @NotNull String title = "";
   private @NotNull String description = "";
   private @NotNull Integer releaseYear = 0;
+
+  public @NotNull String getTitle() {
+    return this.title;
+  }
+
+  public void setTitle(@NotNull String title) {
+    this.title = title;
+  }
+
+  public @NotNull String getDescription() {
+    return this.description;
+  }
+
+  public void setDescription(@NotNull String description) {
+    this.description = description;
+  }
+
+  public @NotNull Integer getReleaseYear() {
+    return this.releaseYear;
+  }
+
+  public void setReleaseYear(@NotNull Integer releaseYear) {
+    this.releaseYear = releaseYear;
+  }
 }
--- src/main/java/org/dcistudent/sakilarest/models/requests/LimitOffsetRequest.java
+++ src/main/java/org/dcistudent/sakilarest/models/requests/LimitOffsetRequest.java
@@ -1,18 +1,30 @@
 package org.dcistudent.sakilarest.models.requests;
 
 import jakarta.validation.constraints.Min;
-import lombok.Getter;
-import lombok.Setter;
 import org.jetbrains.annotations.NotNull;
 
-@Getter
-@Setter
 public class LimitOffsetRequest {
-    @NotNull
-    @Min(10)
-    private Integer limit = 10;
+  @NotNull
+  @Min(10)
+  private Integer limit = 10;
 
-    @NotNull
-    @Min(0)
-    private Integer offset = 0;
+  @NotNull
+  @Min(0)
+  private Integer offset = 0;
+
+  public @NotNull @Min(10) Integer getLimit() {
+    return this.limit;
+  }
+
+  public @NotNull @Min(0) Integer getOffset() {
+    return this.offset;
+  }
+
+  public void setLimit(@NotNull @Min(10) Integer limit) {
+    this.limit = limit;
+  }
+
+  public void setOffset(@NotNull @Min(0) Integer offset) {
+    this.offset = offset;
+  }
 }
