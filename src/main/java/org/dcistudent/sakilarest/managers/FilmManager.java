e870b7551ddd043ad732db220849c708fac6b743
DCI - Marc <marcel.saegebarth@dci-student.org>
2025-08-15 16:53:36 +0200

    Call repository method with specification

--- src/main/java/org/dcistudent/sakilarest/managers/FilmManager.java
+++ src/main/java/org/dcistudent/sakilarest/managers/FilmManager.java
@@ -1,11 +1,13 @@
 package org.dcistudent.sakilarest.managers;
 
 import org.dcistudent.sakilarest.entities.Film;
+import org.dcistudent.sakilarest.models.requests.FilmRequest;
 import org.dcistudent.sakilarest.repositories.FilmRepository;
+import org.dcistudent.sakilarest.specifications.FilmSpecifications;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import java.util.NoSuchElementException;
@@ -20,47 +22,14 @@ public class FilmManager {
     this.filmRepository = filmRepository;
   }
 
-  public @NotNull Page<Film> findAll(@NotNull Integer limit, @NotNull Integer offset) {
-    Pageable pageable = PageRequest.of(offset, limit);
-    return this.filmRepository.findAll(pageable);
-  }
-
   public @NotNull Film findById(@NotNull Long id) {
     return this.filmRepository
         .findById(id)
         .orElseThrow(() -> new NoSuchElementException("Film not found with ID: " + id));
   }
 
-  public @NotNull Page<Film> findByTitle(
-      @NotNull Integer limit,
-      @NotNull Integer offset,
-      @NotNull String pattern
-  ) {
-    Pageable pageable = PageRequest.of(offset, limit);
-    return this.filmRepository
-        .findByTitleContainingIgnoreCase(pattern, pageable)
-        .orElseThrow(() -> new NoSuchElementException("No films found with pattern: " + pattern));
-  }
-
-  public @NotNull Page<Film> findByDescription(
-      @NotNull Integer limit,
-      @NotNull Integer offset,
-      @NotNull String pattern
-  ) {
-    Pageable pageable = PageRequest.of(offset, limit);
-    return this.filmRepository
-        .findByDescription(pattern, pageable)
-        .orElseThrow(() -> new NoSuchElementException("No films found with pattern: " + pattern));
-  }
-
-  public @NotNull Page<Film> findByReleaseYear(
-      @NotNull Integer limit,
-      @NotNull Integer offset,
-      @NotNull Integer pattern
-  ) {
-    Pageable pageable = PageRequest.of(offset, limit);
-    return this.filmRepository
-        .findByReleaseYear(pattern, pageable)
-        .orElseThrow(() -> new NoSuchElementException("No films found with release year: " + pattern));
+  public @NotNull Page<Film> findAll(@NotNull FilmRequest request, @NotNull Pageable pageable) {
+    Specification<Film> specification = FilmSpecifications.toSpecification(request);
+    return this.filmRepository.findAll(specification, pageable);
   }
 }
