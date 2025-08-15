c64b7502b38d61c56730c37f071e0e9de4489816
DCI - Marc <marcel.saegebarth@dci-student.org>
2025-08-15 16:52:53 +0200

    Delombok & get value by fieldname

--- src/main/java/org/dcistudent/sakilarest/models/requests/FilmRequest.java
+++ src/main/java/org/dcistudent/sakilarest/models/requests/FilmRequest.java
@@ -1,14 +1,43 @@
 package org.dcistudent.sakilarest.models.requests;
 
-import lombok.Getter;
-import lombok.Setter;
 import org.jetbrains.annotations.NotNull;
 
-@Getter
-@Setter
 public class FilmRequest extends LimitOffsetRequest {
 
   private @NotNull String title = "";
   private @NotNull String description = "";
   private @NotNull Integer releaseYear = 0;
+
+  public @NotNull Object getRequestValue(@NotNull String fieldName) {
+    return switch (fieldName) {
+      case "title" -> this.getTitle();
+      case "description" -> this.getDescription();
+      case "releaseYear" -> this.getReleaseYear();
+      default -> throw new IllegalStateException("Unexpected value: " + fieldName);
+    };
+  }
+
+  public @NotNull String getTitle() {
+    return this.title;
+  }
+
+  public @NotNull String getDescription() {
+    return this.description;
+  }
+
+  public @NotNull Integer getReleaseYear() {
+    return this.releaseYear;
+  }
+
+  public void setTitle(@NotNull String title) {
+    this.title = title;
+  }
+
+  public void setDescription(@NotNull String description) {
+    this.description = description;
+  }
+
+  public void setReleaseYear(@NotNull Integer releaseYear) {
+    this.releaseYear = releaseYear;
+  }
 }
