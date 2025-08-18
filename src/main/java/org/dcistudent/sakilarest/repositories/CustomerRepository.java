49199d9bca49bbc9cacd2d047123a5a78eb9e017
DCI - Marc <marcel.saegebarth@dci-student.org>
2025-08-18 15:51:26 +0200

    Only query active customers

--- src/main/java/org/dcistudent/sakilarest/repositories/CustomerRepository.java
+++ src/main/java/org/dcistudent/sakilarest/repositories/CustomerRepository.java
@@ -14,9 +14,9 @@ import java.util.UUID;
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
-  @Query("SELECT c FROM Customer c WHERE c.store.uuid = :storeId")
+  @Query("SELECT c FROM Customer c WHERE c.store.uuid = :storeId AND c.active = true")
   @NotNull Optional<Page<Customer>> findCustomersByStoreId(@NotNull UUID storeId, @NotNull Pageable pageable);
 
-  @Query("SELECT c FROM Customer c WHERE c.store.uuid = :storeId AND c.uuid = :customerId")
+  @Query("SELECT c FROM Customer c WHERE c.store.uuid = :storeId AND c.uuid = :customerId AND c.active = true")
   @NotNull Optional<Customer> findCustomerByStoreId(@NotNull UUID storeId, @NotNull UUID customerId);
 }
