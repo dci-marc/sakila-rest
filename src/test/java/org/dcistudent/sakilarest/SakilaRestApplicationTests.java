c44054e8e80e822b1d5fcfe60308faa1b89bf8d7
DCI - Marc <marcel.saegebarth@dci-student.org>
2025-08-19 13:53:45 +0200

    Enable tests only for `test` profile

--- src/test/java/org/dcistudent/sakilarest/SakilaRestApplicationTests.java
+++ src/test/java/org/dcistudent/sakilarest/SakilaRestApplicationTests.java
@@ -3,8 +3,10 @@ package org.dcistudent.sakilarest;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SakilaRestApplicationTests {
 
   @Disabled("Not implemented yet")
