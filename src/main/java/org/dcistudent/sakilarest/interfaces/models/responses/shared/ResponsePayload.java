76731a8be5b9a3fe4fad7c017861ac5dac72a99d
DCI - Marc <marcel.saegebarth@dci-student.org>
2025-08-22 15:12:33 +0200

    Disable `Buildable` interface for now

--- src/main/java/org/dcistudent/sakilarest/interfaces/models/responses/shared/ResponsePayload.java
+++ src/main/java/org/dcistudent/sakilarest/interfaces/models/responses/shared/ResponsePayload.java
@@ -1,4 +1,6 @@
 package org.dcistudent.sakilarest.interfaces.models.responses.shared;
 
-public interface ResponsePayload extends Buildable {
+public interface ResponsePayload {
 }
+// public interface ResponsePayload extends Buildable {
+// }
