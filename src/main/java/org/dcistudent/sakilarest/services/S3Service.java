1ea59b86473f17ebe0af00884f57d53e37545115
DCI - Marc <marcel.saegebarth@dci-student.org>
2025-08-25 12:34:48 +0200

    Implement `S3` service

--- src/main/java/org/dcistudent/sakilarest/services/S3Service.java
+++ src/main/java/org/dcistudent/sakilarest/services/S3Service.java
@@ -1,24 +1,38 @@
 package org.dcistudent.sakilarest.services;
 
+import org.dcistudent.sakilarest.models.requests.s3.S3FileRequest;
 import org.dcistudent.sakilarest.models.responses.domain.s3.Directory;
+import org.dcistudent.sakilarest.models.responses.domain.s3.S3FileServiceResponse;
 import org.dcistudent.sakilarest.models.responses.domain.s3.directories.File;
+import org.dcistudent.sakilarest.models.responses.shared.SuccessResponse;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Service;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.time.Instant;
 import java.util.List;
 
 @Service
 public final class S3Service {
 
-  public Directory getFiles(@NotNull String directoryName) {
+  private final @NotNull S3ClientService service;
+
+  public S3Service(@NotNull S3ClientService service) {
+    this.service = service;
+  }
+
+  /**
+   * Showcase method to simulate fetching files from a directory in S3.
+   */
+  public @NotNull Directory getList(@NotNull String directoryName) {
     Directory dir = new Directory(directoryName, "/");
     List<String> fileNames = List.of("file1.txt", "file2.jpg", "file3.pdf");
 
     fileNames.forEach(fileName -> dir.addFile(
         new File.Builder()
             .setName(fileName)
-            .setSize(1024)
+            .setSize(1024L)
             .setMime("application/octet-stream")
             .setReadable(true)
             .setWritable(true)
@@ -29,5 +43,35 @@ public final class S3Service {
     ));
 
     return dir;
+  }
+
+  public @NotNull SuccessResponse put(@NotNull S3FileRequest request) {
+    return new SuccessResponse.Builder().setSuccess(
+        this.service.upload(
+            String.format("%s/%s", request.getFilePath(), request.getFileName()),
+            new ByteArrayInputStream(request.getBase64Content()),
+            request.getBase64Content().length
+        )
+    ).build();
+  }
+
+  public @NotNull File get(@NotNull String path) throws IOException {
+    S3FileServiceResponse response = this.service.download(path);
+
+    return new File.Builder()
+        .setName(String.valueOf(response.getResponse().metadata().get(S3ClientService.HEADER_FILENAME)))
+        .setSize(response.getResponse().contentLength())
+        .setMime(response.getResponse().contentType())
+        .setBase64Content(response.getContent())
+        .setReadable(true)
+        .setWritable(true)
+        .setDeletable(true)
+        .setModifiedAt(response.getResponse().lastModified())
+        .setCreatedAt(response.getResponse().lastModified())
+        .build();
+  }
+
+  public @NotNull SuccessResponse delete(@NotNull String path) {
+    return new SuccessResponse.Builder().setSuccess(this.service.delete(path)).build();
   }
 }
