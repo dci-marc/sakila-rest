a2cbd03083ca68e35e4ed9e664f7bf560afdc658
DCI - Marc <marcel.saegebarth@dci-student.org>
2025-08-26 10:06:07 +0200

    Set fields color in `Discord` model

--- src/main/java/org/dcistudent/sakilarest/models/requests/shared/discord/Discord.java
+++ src/main/java/org/dcistudent/sakilarest/models/requests/shared/discord/Discord.java
@@ -3,16 +3,19 @@ package org.dcistudent.sakilarest.models.requests.shared.discord;
 import org.dcistudent.sakilarest.interfaces.models.responses.shared.Buildable;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public final class Discord {
 
   private final @NotNull String username;
   private final @NotNull List<Embed> embeds;
+  private @NotNull Integer fieldsColor;
 
   public Discord(@NotNull Builder builder) {
     this.username = builder.username;
     this.embeds = builder.embeds;
+    this.fieldsColor = 0;
   }
 
   public @NotNull String getUsername() {
@@ -23,9 +26,18 @@ public final class Discord {
     return this.embeds;
   }
 
+  public @NotNull Integer getFieldsColor() {
+    return this.fieldsColor;
+  }
+
+  public @NotNull Discord setFieldsColor(@NotNull Integer color) {
+    this.fieldsColor = color;
+    return this;
+  }
+
   public static final class Builder implements Buildable<Discord> {
     private @NotNull String username = "";
-    private @NotNull List<Embed> embeds = List.of();
+    private @NotNull List<Embed> embeds = new ArrayList<>();
 
     public @NotNull Builder setUsername(@NotNull String username) {
       this.username = username;
@@ -33,7 +45,12 @@ public final class Discord {
     }
 
     public @NotNull Builder setEmbeds(@NotNull List<Embed> embeds) {
-      this.embeds = embeds;
+      embeds.forEach(this::addEmbed);
+      return this;
+    }
+
+    private @NotNull Builder addEmbed(@NotNull Embed embed) {
+      this.embeds.add(embed);
       return this;
     }
 
