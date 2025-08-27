64d73f7130d75523d8f4b19d7d598a7de4a1eefc
DCI - Marc <marcel.saegebarth@dci-student.org>
2025-08-27 12:32:22 +0200

    Return `EmptyResponse` instance instead

--- src/main/java/org/dcistudent/sakilarest/exceptions/domain/Auth0Exception.java
+++ src/main/java/org/dcistudent/sakilarest/exceptions/domain/Auth0Exception.java
@@ -1,8 +1,8 @@
 package org.dcistudent.sakilarest.exceptions.domain;
 
 import org.dcistudent.sakilarest.exceptions.shared.ApiException;
-import org.dcistudent.sakilarest.interfaces.models.responses.shared.ResponsePayload;
 import org.dcistudent.sakilarest.models.responses.error.Auth0ErrorResponse;
+import org.dcistudent.sakilarest.models.responses.shared.EmptyResponse;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -13,11 +13,7 @@ public final class Auth0Exception extends ApiException {
   private final transient @NotNull Auth0ErrorResponse error;
 
   public Auth0Exception(Auth0ErrorResponse error) {
-    super(
-        error.getMessage(),
-        new ResponsePayload() {
-        }
-    );
+    super(error.getMessage(), EmptyResponse.INSTANCE);
     this.error = error;
   }
 
