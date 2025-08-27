d052ee4cab70d040b77cc39c039d57f8a7688e24
DCI - Marc <marcel.saegebarth@dci-student.org>
2025-08-27 12:15:41 +0200

    Implement `ApiException`

--- src/main/java/org/dcistudent/sakilarest/exceptions/domain/Auth0Exception.java
+++ src/main/java/org/dcistudent/sakilarest/exceptions/domain/Auth0Exception.java
@@ -1,14 +1,23 @@
 package org.dcistudent.sakilarest.exceptions.domain;
 
+import org.dcistudent.sakilarest.exceptions.shared.ApiException;
+import org.dcistudent.sakilarest.interfaces.models.responses.shared.ResponsePayload;
 import org.dcistudent.sakilarest.models.responses.error.Auth0ErrorResponse;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
-public final class Auth0Exception extends RuntimeException {
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public final class Auth0Exception extends ApiException {
 
   private final transient @NotNull Auth0ErrorResponse error;
 
   public Auth0Exception(Auth0ErrorResponse error) {
-    super(error.getMessage());
+    super(
+        error.getMessage(),
+        new ResponsePayload() {
+        }
+    );
     this.error = error;
   }
 
