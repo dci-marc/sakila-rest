bd7cc480dc9502d2854819d8ed2c9c411174e9da
DCI - Marc <marcel.saegebarth@dci-student.org>
2025-08-27 15:12:34 +0200

    Implement `Serializable`

--- src/main/java/org/dcistudent/sakilarest/models/responses/shared/AbstractUuidResponse.java
+++ src/main/java/org/dcistudent/sakilarest/models/responses/shared/AbstractUuidResponse.java
@@ -2,9 +2,10 @@ package org.dcistudent.sakilarest.models.responses.shared;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.io.Serializable;
 import java.util.UUID;
 
-public abstract class AbstractUuidResponse {
+public abstract class AbstractUuidResponse implements Serializable {
 
   @NotNull UUID uuid;
 
