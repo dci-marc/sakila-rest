8e1caaf26ce12cc25958896f7a9b3217646fc79b
DCI - Marc <marcel.saegebarth@dci-student.org>
2025-08-29 14:31:46 +0200

    Don't check for empty list in object

--- src/main/java/org/dcistudent/sakilarest/models/responses/domain/s3/S3DirectoryServiceResponse.java
+++ src/main/java/org/dcistudent/sakilarest/models/responses/domain/s3/S3DirectoryServiceResponse.java
@@ -2,29 +2,26 @@ package org.dcistudent.sakilarest.models.responses.domain.s3;
 
 import org.jetbrains.annotations.NotNull;
 
-import java.util.List;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 public final class S3DirectoryServiceResponse {
 
-  private @NotNull List<S3FileServiceResponse> files;
+  private final @NotNull Set<S3FileServiceResponse> files;
 
   public S3DirectoryServiceResponse(@NotNull Builder builder) {
-    if (builder.files.isEmpty()) {
-      throw new IllegalArgumentException("Files cannot be null");
-    }
     this.files = builder.files;
   }
 
+  public @NotNull Set<S3FileServiceResponse> getFiles() {
+    return this.files;
+  }
+
   public static final class Builder {
-    private @NotNull List<S3FileServiceResponse> files = List.of();
+    private final @NotNull Set<S3FileServiceResponse> files = new LinkedHashSet<>();
 
     public void addFile(@NotNull S3FileServiceResponse file) {
       this.files.add(file);
-    }
-
-    public @NotNull Builder setFiles(@NotNull List<S3FileServiceResponse> files) {
-      this.files = files;
-      return this;
     }
 
     public S3DirectoryServiceResponse build() {
